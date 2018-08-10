0e069877201c3e7d6e655f1bda898abdb6c8ea30
jchanme <39323414+jchanme@users.noreply.github.com>
2018-08-10 23:09:04 +0800

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "External Layout Customize",
+    'author': "Safyric Co., Ltd.",
+    'website': "https://www.safyric.com",
+    'version': '0.1',
+
+        'depends': [
+        'base',
+    ],
+
+    'data': [
+        'views/external_layout_template.xml',
+    ],
+    'installable': True,
+}
